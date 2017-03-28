cae45c4e43129a3b75aa83f69503b5e2e361e886
Ahmed Toulan <atoulan@atoulan-laptop>
2017-03-28 12:58:47 +0200

    Refactoring the install script.

--- install.sh
+++ install.sh
@@ -7,71 +7,46 @@ info() {
   printf '\e[34m'; echo "$@"; printf '\E[0m'
 }
 
-#if [[ $EUID -eq 0 ]]; then
-if [ "$(id -u)" != "0" ]; then
-    error "This script should be run using sudo or as the root user"
-    exit 1
-fi
+create_crt(){
+  mkdir -p /usr/local/certs/
+  if [ ! -f /usr/local/certs/server.crt ]; then
+      info "Generating self signed certificate ..."
+      openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /usr/local/certs/server.key -out /usr/local/certs/server.crt
+      info "Certificate created!"
+  fi
+}
 
-if [ "$1" == "" ]; then
-    error "Please provide google cloud access key"
-    exit 1
-fi
 
-if [ "$2" == "" ]; then
-	error "Please provide instance id"
-	exit
-fi
+add_google_repo(){
+  # Create an environment variable for the correct distribution
+  export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+  
+  # Add the Cloud SDK distribution URI as a package source
+  rm /etc/apt/sources.list.d/google-cloud-sdk.list || true
+  
+  echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+  
+  # Import the Google Cloud Platform public key
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+}
 
-if [ ! -f "$1" ]; then
-    error "Key not found!"
-    exit 1
-fi
+install_deps(){
+  apt-get update && apt-get install -y libssl-dev supervisor wget mongodb-server openssl curl google-cloud-sdk 
+}
 
-mkdir -p /usr/local/certs/
-if [ ! -f /usr/local/certs/server.crt ]; then
-    info "Generating self signed certificate ..."
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /usr/local/certs/server.key -out /usr/local/certs/server.crt
-    info "Certificate created!"
-fi
+pull_proxy(){
+  # fetch the proxy program (binary file)
+  gcloud auth activate-service-account --key-file ${GS_KEY}
+  
+  gsutil -m cp gs://pos-proxy/staging/bin/pos-proxy .
+  
+  mkdir -p /usr/local/bin
+  cp ./pos-proxy /usr/local/bin/pos-proxy
+  
+  chmod +x /usr/local/bin/pos-proxy
+}
 
-GS_KEY=$1
-
-# install mongodb
-apt-get install -y libssl-dev python-pip supervisor wget mongodb-server openssl curl
-
-service supervisor restart
-
-#service mongodb enable
-service mongodb start
-
-# install gsutil
-pip install pyopenssl
-# Create an environment variable for the correct distribution
-export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-
-# Add the Cloud SDK distribution URI as a package source
-rm /etc/apt/sources.list.d/google-cloud-sdk.list || true
-
-echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-
-# Import the Google Cloud Platform public key
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-
-# Update the package list and install the Cloud SDK
-apt-get update && apt-get install -y google-cloud-sdk
-
-# fetch the proxy program (binary file)
-gcloud auth activate-service-account --key-file ${GS_KEY}
-
-gsutil -m cp gs://pos-proxy/staging/bin/pos-proxy .
-pwd
-
-mkdir -p /usr/local/bin
-cp ./pos-proxy /usr/local/bin/pos-proxy
-
-chmod +x /usr/local/bin/pos-proxy
-
+write_config(){
 # create supervisor configuration file
 FILE=/etc/supervisor/conf.d/pos_proxy.conf
 touch $FILE
@@ -106,4 +81,35 @@ cat <<EOM >$FILE
      ]
 }
 EOM
+}
+
+if [ "$(id -u)" != "0" ]; then
+    error "This script should be run using sudo or as the root user"
+    exit 1
+fi
+
+if [ "$1" == "" ]; then
+    error "Please provide google cloud access key"
+    exit 1
+fi
+
+if [ "$2" == "" ]; then
+	error "Please provide instance id"
+	exit
+fi
+
+if [ ! -f "$1" ]; then
+    error "Key not found!"
+    exit 1
+fi
+
+GS_KEY=$1
+
+create_crt
+apt-get install curl
+add_google_repo
+install_deps
+pull_proxy
+write_config
+sudo systemctl restart mongodb.service
 sudo systemctl restart supervisor.service
