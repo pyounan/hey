92cf4f70f733f062493953c4ea4575a42cabfb21
Eslam Mostafa <cseslam@gmail.com>
2017-09-06 17:55:35 +0200

    try setting origin

--- proxy/general.go
+++ proxy/general.go
@@ -1,9 +1,13 @@
 package proxy
 
 import (
+	//"time"
+	//"fmt"
 	"encoding/json"
 	"pos-proxy/config"
+	//"pos-proxy/helpers"
 	"net/http"
+	//"io/ioutil"
 	"net/http/httputil"
 	"net/url"
 )
@@ -17,6 +21,32 @@ func ProxyToBackend(w http.ResponseWriter, r *http.Request) {
 	prox := httputil.NewSingleHostReverseProxy(backendURI)
 	r.SetBasicAuth(config.AuthUsername, config.AuthPassword)
 	r.Header.Del("Access-Control-Allow-Origin")
+	r.Header.Del("Origin")
+	r.Header.Set("Origin", w.Header().Get("Origin"))
 	w.Header().Del("Access-Control-Allow-Origin")
 	prox.ServeHTTP(w, r)
 }
+
+/*func ProxyToBackend(w http.ResponseWriter, r *http.Request) {
+	// backendURI, _ := url.Parse(config.Config.BackendURI)
+	netClient := &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	uri := fmt.Sprintf("%s%s", config.Config.BackendURI, r.RequestURI)
+	req, err := http.NewRequest(r.Method, uri, r.Body)
+	// req.Host = config.Config.BackendURI
+	req = helpers.PrepareRequestHeaders(r)
+	resp, err := netClient.Do(req)
+	if err != nil {
+		helpers.ReturnErrorMessage(w, err)
+		return
+	}
+	respbody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		helpers.ReturnErrorMessage(w, err)
+		return
+	}
+	defer resp.Body.Close()
+	w.Write(respbody)
+}*/
