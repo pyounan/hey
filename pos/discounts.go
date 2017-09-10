7702a447f40c9e1b4041ca686e04b2e0f15ab611
Eslam Mostafa <cseslam@gmail.com>
2017-09-10 23:25:02 +0200

    fix query map of fixeddiscounts api

--- pos/discounts.go
+++ pos/discounts.go
@@ -4,18 +4,26 @@ import (
 	"net/http"
 	"pos-proxy/db"
 	"pos-proxy/helpers"
+	"strconv"
 
 	"gopkg.in/mgo.v2/bson"
 )
 
+// ListFixedDiscounts returns a list of fixed discounts for a store
 func ListFixedDiscounts(w http.ResponseWriter, r *http.Request) {
 	query := bson.M{}
-	queryParams := r.URL.Query()
-	for key, val := range queryParams {
-		query[key] = val
+	urlQuery := r.URL.Query()
+	if _, ok := urlQuery["store"]; ok {
+		id, _ := strconv.Atoi(urlQuery["store"][0])
+		query["stores"] = id
 	}
+	if _, ok := urlQuery["cashier"]; ok {
+		id, _ := strconv.Atoi(urlQuery["cashier"][0])
+		query["cashiers"] = id
+	}
+
 	fixedDiscounts := []map[string]interface{}{}
-	err := db.DB.C("fixeddiscounts").Find(query).All(&fixedDiscounts)
+	err := db.DB.C("fixeddiscounts").Find(query).Sort("id").All(&fixedDiscounts)
 	if err != nil {
 		helpers.ReturnErrorMessage(w, err.Error())
 		return
