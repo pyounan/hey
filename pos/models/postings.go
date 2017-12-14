4394875827cb1d13020c4fc2f9a69895606734d9
Ahmed Toulan <atoulan@cloudinn.net>
2017-12-14 15:40:39 +0200

    Make RoomNumber and CurrencyID integer and nullable.

--- pos/models/postings.go
+++ pos/models/postings.go
@@ -9,17 +9,17 @@ type Posting struct {
 	Cashier                int        `json:"cashier" bson:"cashier"`
 	CashierID              int        `json:"cashier_id" bson:"cashier_id"`
 	Comments               string     `json:"comments" bson:"comments"`
-	CurrencyID             int        `json:"currency_id" bson:"currency_id"`
+	CurrencyID             *int64     `json:"currency_id" bson:"currency_id"`
 	Currency               int        `json:"currency" bson:"currency"`
 	CurrencyDetails        string     `json:"currency_details" bson:"currency_details"`
-	Department             int        `json:"department,omitempty" bson:"department,omitempty"`
+	Department             int        `json:"department" bson:"department"`
 	DepartmentDetails      string     `json:"department_details" bson:"department_details"`
 	ForeignAmount          float64    `json:"foreign_amount" bson:"foreign_amount"`
 	FrontendID             string     `json:"frontend_id" bson:"frontend_id"`
 	PosinvoiceID           int        `json:"posinvoice_id" bson:"posinvoice_id"`
 	PostingType            string     `json:"posting_type" bson:"posting_type"`
 	Room                   *int64     `json:"room" bson:"room"`
-	RoomNumber             string     `json:"room_number" bson:"room_number"`
+	RoomNumber             *int64     `json:"room_number" bson:"room_number"`
 	RoomDetails            *string    `json:"room_details" bson:"room_details"`
 	PosPostingInformations []Posting  `json:"pospostinginformations" bson:"pospostinginformations"`
 	PaymentLog             PaymentLog `json:"paymentlog" bson:"paymentlog"`
