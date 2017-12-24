628c6b9108d8bb5f4af4355e0aeba2cbc1cb37d7
Ahmed Toulan <atoulan@cloudinn.net>
2017-12-24 12:42:27 +0200

    Check pos-proxy#27. Move fetching token to auth.go

--- auth/auth.go
+++ auth/auth.go
@@ -1,13 +1,21 @@
 package auth
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"gopkg.in/mgo.v2/bson"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"pos-proxy/config"
 	"pos-proxy/db"
 	"pos-proxy/helpers"
 	"strconv"
+	"strings"
 )
+
+var Token string
 
 func EnsureTenantSelected(w http.ResponseWriter, req *http.Request) {
 	helpers.ReturnSuccessMessage(w, true)
@@ -26,4 +34,28 @@ func GetUserPermissions(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 	helpers.ReturnSuccessMessage(w, perms["permissions"])
+}
+
+func FetchToken() {
+	type TokenResponse struct {
+		Token string `json:"token"`
+	}
+	netClient := helpers.NewNetClient()
+	uri := fmt.Sprintf("%s%s", config.Config.BackendURI, "/api/is_authenticated/")
+	req, err := http.NewRequest("GET", uri, strings.NewReader(""))
+	req = helpers.PrepareRequestHeaders(req)
+	resp, err := netClient.Do(req)
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Failed to read body", err.Error())
+	}
+	data := TokenResponse{}
+	err = json.Unmarshal(respBody, &data)
+	if err != nil {
+		log.Println("Failed to parse update data", string(respBody), err.Error())
+	}
+
+	log.Println("resp", data.Token)
+	Token = data.Token
+	log.Println("Token", Token)
 }
