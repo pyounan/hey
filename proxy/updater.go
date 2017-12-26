3f5cc100eb1621d1d9f6a8f1672e68577053d3f7
Ahmed Toulan <atoulan@cloudinn.net>
2017-12-26 13:09:15 +0200

    Revert "Spawn using bash scripts."
    This reverts commit 9a1ff1aef7f046e9577d274d56397f90caa9a7db.

--- proxy/updater.go
+++ proxy/updater.go
@@ -77,23 +77,19 @@ func initiateUpdate(buildNumber int64) error {
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
-
 	log.Println("Will download")
-
 	err = cmd.Run()
 	if err != nil {
 		log.Println("Failed to fetch update script")
 		os.RemoveAll(dir)
 		return err
 	}
-
 	err = os.Chmod(dir, 0777)
 	if err != nil {
 		log.Println("Failed to chmod on update folder", err.Error())
 		os.RemoveAll(dir)
 		return err
 	}
-
 	updateCommand := fmt.Sprintf("%s/update.sh", dir)
 	err = os.Chmod(updateCommand, 0777)
 	if err != nil {
@@ -101,39 +97,14 @@ func initiateUpdate(buildNumber int64) error {
 		os.RemoveAll(dir)
 		return err
 	}
-
-	runnerCommand := fmt.Sprintf("%s/runner.sh")
-	f, err := os.Create(runnerCommand)
-	if err != nil {
-		log.Println("Couldn't create runner", err.Error())
-		os.RemoveAll(dir)
-		return err
-	}
-
-	err = os.Chmod(runnerCommand, 0777)
-	if err != nil {
-		log.Println("Failed to chmod on runner command", err.Error())
-		os.RemoveAll(dir)
-		return err
-	}
-
-	_, err = f.WriteString("#!/bin/bash\n./update.sh &")
-	if err != nil {
-		log.Println("Failed to write to runner file", err.Error())
-		os.RemoveAll(dir)
-		return err
-	}
-	f.Sync()
-	f.Close()
-
 	log.Println("Done Downloading")
-	update(buildNumber, runnerCommand, dir)
+	update(buildNumber, updateCommand, dir)
 	return nil
 }
 
-func update(buildNumber int64, runnerCommand, updateDir string) {
+func update(buildNumber int64, updateCommand, updateDir string) {
 	log.Println("Starting update process")
-	cmd := exec.Command(runnerCommand, config.VirtualHost, fmt.Sprintf("%d", buildNumber), updateDir)
+	cmd := exec.Command(updateCommand, config.VirtualHost, fmt.Sprintf("%d", buildNumber), updateDir)
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
