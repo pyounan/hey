82c5e0e3cf890924695a30802f1361a44244c717
Mohammad Allam <mallam@cloudinn.net>
2018-05-16 15:34:19 +0200

    Fix CI setting ACL permission on the right bucket when publish proxy to it

--- ci/scripts/release.sh
+++ ci/scripts/release.sh
@@ -14,4 +14,4 @@ gcloud config set project corded-guild-155314
 
 # Publish build
 gsutil -m cp -r "gs://pos-proxy/test/${BUILD_ID}/*" "gs://pos-proxy/${TARGET_ENV}/${BUILD_ID}/"
-gsutil -m acl -r ch -u AllUsers:R "gs://pos-proxy/staging/${BUILD_ID}/*"
+gsutil -m acl -r ch -u AllUsers:R "gs://pos-proxy/${TARGET_ENV}/${BUILD_ID}/*"
